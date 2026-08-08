99592e25bb9f6b1d16713eab41ab2bf83ad67b79
StevenFlores <sflores021403@gmail.com>
2026-08-08 13:10:28 -0500

    Create first FastAPI database

--- app/main.py
+++ app/main.py
@@ -2,7 +2,28 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+equipment = [
+    {
+        "id": 1,
+        "serial_number": "220H00192",
+        "type": "ISCO GLS Sampler",
+        "status": "storage",
+        "hub": "St. Louis"
+    },
+    {
+        "id": 2,
+        "serial_number": "220H00204",
+        "type": "ISCO GLS Sampler",
+        "status": "storage",
+        "hub": "Knoxville"
+    }
+]
+
 
 @app.get("/")
 def home():
-    return {"message": "Pilot Program system online"}
+    return {"message": "Pilot Program system online."}
+
+@app.get("/equipment")
+def get_equipment():
+    return equipment
